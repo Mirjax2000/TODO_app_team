9a11db73dc8d279dae5e2a02b2f559ecde1bb0f3
DaliborZ-tech <dalibor.zalesak@gmail.com>
2024-08-05 20:47:33 +0200

    modifikace main, odebrán title úkolu, přidáno ID úkolu a status úkolu

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ import os
 import csv
 
 class Task:
-    def __init__(self, title, description):
-        self.title = title
+    def __init__(self, ID, description, status):
+        self.ID = ID
         self.description = description
+        self.status = status
 
     def __str__(self):
-        return f"Task(title={self.title}, description={self.description})"
+        return f"Task(ID={self.ID}, description={self.description}, status={self.status})"
 
 class TaskManager:
     def __init__(self):
@@ -20,10 +21,10 @@ class TaskManager:
         file_path = os.path.join(os.path.dirname(__file__), 'import_tasks.json')
         with open(file_path, 'r', encoding='utf-8') as file:
             data = json.load(file)
-            self.tasks = [Task(item['title'], item['description']) for item in data]
+            self.tasks = [Task(item['ID'], item['description'], item['status']) for item in data]
 
     def get_task_list(self):
-        return [(index, task.title, task.description) for index, task in enumerate(self.tasks)]
+        return [(task.ID, task.description, task.status) for index, task in enumerate(self.tasks)]
 
 
 # pro testovací výpis:
