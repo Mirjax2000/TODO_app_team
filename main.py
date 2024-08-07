9a468c15ba8759696cfe21893fc10ba99fe75eeb
DaliborZ-tech <dalibor.zalesak@gmail.com>
2024-08-07 16:06:57 +0200

    přidán export do csv

--- main.py
+++ main.py
@@ -23,6 +23,14 @@ class TaskManager:
             data = json.load(file)
             self.tasks = [Task(item['ID'], item['description'], item['status']) for item in data]
 
+    def save_tasks_to_csv(self):
+        file_path = os.path.join(os.path.dirname(__file__), "tasks.csv")
+        with open(file_path, "w", newline="", encoding="utf-8") as file:
+            writer = csv.writer(file)
+            writer.writerow(["Description", "Status"])
+            for task in self.tasks:
+                writer.writerow([task.description, task.status])
+
     def add_task(self, ID, description, status):
         new_task = Task(ID, description, status)
         self.tasks.append(new_task)
@@ -30,11 +38,14 @@ class TaskManager:
     def get_task_list(self):
         return [(task.ID, task.description, task.status) for task in self.tasks]
 
-
 # pro testovací výpis:
-manager = TaskManager()
-manager.load_tasks_from_json()
-manager.add_task("4", "Popis úkolu 4.", "False")
+if __name__ == "__main__":
+    manager = TaskManager()
+    manager.load_tasks_from_json()
+    manager.add_task("4", "Popis úkolu 4.", "False")
 
-for task in manager.get_task_list():
-    print(type(task))
+    for task in manager.get_task_list():
+        print(task)
+
+    manager.save_tasks_to_csv()
+    print("Tasks have been saved to tasks.csv")
