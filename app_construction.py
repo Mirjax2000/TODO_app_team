194936f376d4fd720aa292b000c871849ce89f71
Vigo <miroslav.viktorin@seznam.cz>
2024-08-17 17:52:17 +0200

    makame

--- app_construction.py
+++ app_construction.py
@@ -1,5 +1,5 @@
 import customtkinter as ctk
-from main import TaskManager
+from main import TaskManager, Task
 from PIL import Image
 
 # variables
@@ -7,6 +7,10 @@ font_big: tuple[str, int, str] = ("Arial", 30, "normal")
 font_normal: tuple[str, int, str] = ("Arial", 20, "normal")
 font_small: tuple[str, int, str] = ("Arial", 16, "normal")
 version: float = 0.5
+
+img_error = ctk.CTkImage(
+    light_image=Image.open("./assets/exclamation2.png"), size=(30, 30)
+)
 
 inner_color: str = "#292929"
 outer_color: str = "#1c1c1c"
@@ -38,9 +42,8 @@ ctk.set_widget_scaling(1.0)
 class App(ctk.CTk):
     """Main App"""
 
-    # constructor
-    def __init__(self):
-
+    def __init__(self, task_manager):
+        self.task_manager: TaskManager = task_manager
         super().__init__()
         self.title("TODO-List")
         self.iconbitmap("./assets/ico.ico")
@@ -49,114 +52,92 @@ class App(ctk.CTk):
         self.configure(fg_color=outer_color)
 
         # Frames
-        self.task = TaskManager(self)
-        self.header = Header(self)
-        self.display = Display(self)
-        self.footer = Footer(self)
+        # Header Frame
+        self.header = ctk.CTkFrame(self, fg_color=outer_color)
+        self.header.pack(side="top", fill="x", padx=20, pady=20)
 
-    def center_window(self):
-        """Centers the window on the screen."""
-
-        self.update_idletasks()
-        width: int = 1000
-        height: int = 600
-        screen_width: int = self.winfo_screenwidth()
-        screen_height: int = self.winfo_screenheight()
-        x: int = screen_width // 2 - width // 2
-        y: int = screen_height // 2 - height // 2
-        self.geometry(f"{width}x{height}+{x}+{y}")
-
-
-class Header(ctk.CTkFrame):
-    """Frame pro hlavičku"""
-
-    def __init__(self, parent):
-        self.parent = parent
-        super().__init__(parent, fg_color=outer_color)
-        self.pack(side="top", fill="x", padx=20, pady=20)
-
-        # Vstupní pole pro zadání úkolu
+        # Entre field for input task
         self.input_task = ctk.CTkEntry(
-            self,
+            self.header,
             placeholder_text="Enter a task",
             font=font_normal,
             border_width=1,
             border_color=border_color,
         )
         self.input_task.get()
-        self.input_task.bind("<Return>", self.parent.task.add_task)
+        # self.input_task.bind("<Return>", self.add_task) # Todo dopln Funkci
         self.input_task.grid(row=0, column=0, padx=(0, 20), sticky="ew")
 
-        # Tlačítko pro přidání úkolu
-        self.input_btn = ctk.CTkButton(
-            self,
+        # # Tlačítko pro přidání úkolu
+        self.input_button = ctk.CTkButton(
+            self.header,
             text="Add Task",
             font=font_normal,
-            # command=,
+            # command=, # Todo dopln funkci
         )
-        self.input_btn.grid(row=0, column=1, sticky="e")
+        self.input_button.grid(row=0, column=1, sticky="e")
 
-        # Grid konfigurace headru
-        self.columnconfigure(0, weight=1, uniform="a")
-        self.columnconfigure(1, weight=0, uniform="b")
-        self.rowconfigure(0, weight=0, uniform="a")
+        # # Grid konfigurace headru
+        self.header.columnconfigure(0, weight=1, uniform="a")
+        self.header.columnconfigure(1, weight=0, uniform="b")
+        self.header.rowconfigure(0, weight=0, uniform="a")
 
-
-class Display(ctk.CTkFrame):
-    """Frame pro zobrazeni seznamu úkolů"""
-
-    def __init__(self, parent):
-        self.parent = parent
         # a main frame: body
-        super().__init__(parent, fg_color=outer_color)
-        self.pack(side="top", fill="both", padx=20, expand=True)
+        self.display = ctk.CTkFrame(self, fg_color=outer_color)
+        self.display.pack(side="top", fill="both", padx=20, expand=True)
         # left btn frame
         self.display_frame = ctk.CTkFrame(
-            self, fg_color=inner_color, border_width=1, border_color=border_color
+            self.display,
+            fg_color=inner_color,
+            border_width=1,
+            border_color=border_color,
         )
-        # right btn frame
-        self.display_btns = ctk.CTkFrame(self, fg_color=outer_color, width=140)
-        # aktivace widgetu pro right and left frame
+        #
+        # # right btn frame
+        self.display_buttons = ctk.CTkFrame(
+            self.display, fg_color=outer_color, width=140
+        )
+        # right and left frame activated
         self.display_frame.grid(row=0, column=0, sticky="nsew")
-        self.display_btns.grid(row=0, column=1, sticky="nsew", padx=(20, 0))
-        # grid config pravy a levy grid
-        self.columnconfigure(0, weight=1, uniform="a")
-        self.columnconfigure(1, weight=0, uniform="b")
-        self.rowconfigure(0, weight=1, uniform="c")
-
-        # buttons framy top, mid, bottom
-        self.btn_frame = {  # config pro vsechny tlacitka
-            "master": self.display_btns,  # kdo je rodic
+        self.display_buttons.grid(row=0, column=1, sticky="nsew", padx=(20, 0))
+        # right and left frame configure
+        self.display.columnconfigure(0, weight=1, uniform="a")
+        self.display.columnconfigure(1, weight=0, uniform="b")
+        self.display.rowconfigure(0, weight=1, uniform="c")
+        #
+        # buttons frames top, mid, bottom
+        self.button_frame = {  # config for all buttons
+            "master": self.display_buttons,  # parent
             "width": 140,
             "fg_color": outer_color,
         }
-        # tlacitka v top
-        self.display_btns_top = ctk.CTkFrame(**self.btn_frame)
-        # tlacitka v mid
-        self.display_btns_mid = ctk.CTkFrame(**self.btn_frame)
-        # tlacitka v bottom
-        self.display_btns_btm = ctk.CTkFrame(**self.btn_frame)
+        # Buttons in top
+        self.display_buttons_top = ctk.CTkFrame(**self.button_frame)
+        # Buttons in mid
+        self.display_buttons_mid = ctk.CTkFrame(**self.button_frame)
+        # Buttons in bottom
+        self.display_buttons_btm = ctk.CTkFrame(**self.button_frame)
         # activace widgetu pro btn framy
-        self.display_btns_top.grid(row=0, column=0, sticky="ns")
-        self.display_btns_mid.grid(row=1, column=0, sticky="ns", pady=20)
-        self.display_btns_btm.grid(row=2, column=0, sticky="s")
+        self.display_buttons_top.grid(row=0, column=0, sticky="ns")
+        self.display_buttons_mid.grid(row=1, column=0, sticky="ns", pady=20)
+        self.display_buttons_btm.grid(row=2, column=0, sticky="s")
         #
         # grid pr tlacitkovy framy
-        self.display_btns.rowconfigure(0, weight=1, uniform="a")
-        self.display_btns.rowconfigure(1, weight=1, uniform="b")
-        self.display_btns.rowconfigure(2, weight=1, uniform="c")
-        self.display_btns.columnconfigure(0, weight=0, uniform="a")
+        self.display_buttons.rowconfigure(0, weight=1, uniform="a")
+        self.display_buttons.rowconfigure(1, weight=1, uniform="b")
+        self.display_buttons.rowconfigure(2, weight=1, uniform="c")
+        self.display_buttons.columnconfigure(0, weight=0, uniform="a")
         #
         # Create buttons dynamically
-        path = parent.task
+        path = self.task_manager
         self.button_configs = [
-            (self.display_btns_top, btn_text[0], path.remove_task, "btn_1"),
-            (self.display_btns_top, btn_text[1], path.edit_task, "btn_2"),
-            (self.display_btns_mid, btn_text[2], path.load_list, "btn_3"),
-            (self.display_btns_mid, btn_text[3], path.save_list, "btn_4"),
-            (self.display_btns_mid, btn_text[4], path.extend_list, "btn_5"),
-            (self.display_btns_mid, btn_text[5], path.clear_list, "btn_6"),
-            (self.display_btns_btm, btn_text[6], path.exit, "btn_7"),
+            (self.display_buttons_top, btn_text[0], path.remove_task, "btn_1"),
+            (self.display_buttons_top, btn_text[1], path.edit_task, "btn_2"),
+            (self.display_buttons_mid, btn_text[2], path.load_list, "btn_3"),
+            (self.display_buttons_mid, btn_text[3], path.extend_list, "btn_4"),
+            (self.display_buttons_mid, btn_text[4], path.save_list, "btn_5"),
+            (self.display_buttons_mid, btn_text[5], path.clear_list, "btn_6"),
+            (self.display_buttons_btm, btn_text[6], path.exit, "btn_7"),
         ]
         #
         for parent, text, command, attr_name in self.button_configs:
@@ -167,11 +148,11 @@ class Display(ctk.CTkFrame):
             button = getattr(self, f"btn_{i + 1}")
             button.grid(row=i + 1, column=0, sticky="n", pady=1)
         # clear list config
-        self.display_btns_mid.rowconfigure(5, weight=1, uniform="a")
+        self.display_buttons_mid.rowconfigure(5, weight=1, uniform="a")
 
         # label task operations
         self.label_tasks = ctk.CTkLabel(
-            self.display_btns_top,
+            self.display_buttons_top,
             font=font_small,
             text="Task operations",
             fg_color=inner_color,
@@ -180,7 +161,7 @@ class Display(ctk.CTkFrame):
 
         # label list operations
         self.label_lists = ctk.CTkLabel(
-            self.display_btns_mid,
+            self.display_buttons_mid,
             font=font_small,
             text="List operations",
             fg_color=inner_color,
@@ -190,28 +171,23 @@ class Display(ctk.CTkFrame):
         self.label_tasks.grid(row=0, column=0, sticky="ew")
         self.label_lists.grid(row=0, column=0, sticky="ew")
         # label config
-        self.display_btns_top.rowconfigure(0, weight=0, uniform="b")
-        self.display_btns_mid.rowconfigure(0, weight=0, uniform="b")
+        self.display_buttons_top.rowconfigure(0, weight=0, uniform="b")
+        self.display_buttons_mid.rowconfigure(0, weight=0, uniform="b")
 
-
-class Footer(ctk.CTkFrame):
-    """spodni frame pro text a tlacitko pro ulozeni seznamu"""
-
-    def __init__(self, parent):
-        super().__init__(parent, fg_color=outer_color)
-        self.parent = parent
-        self.pack(side="bottom", fill="x", pady=20, padx=20)
+        #  Footer frame
+        self.footer = ctk.CTkFrame(self, fg_color=outer_color)
+        self.footer.pack(side="bottom", fill="x", pady=20, padx=20)
 
         # Vstupní pole pro zadání jména seznamu
         self.footer_label = ctk.CTkLabel(
-            self,
+            self.footer,
             font=font_normal,
             text="List name: ",
         )
 
         # Vstupní pole pro zadání jména seznamu
         self.footer_entry = ctk.CTkEntry(
-            self,
+            self.footer,
             width=300,
             font=font_normal,
             placeholder_text="List name: ",
@@ -221,20 +197,58 @@ class Footer(ctk.CTkFrame):
         )
         self.footer_entry.get()
 
-        # error label
+        # error label + img
+
         self.error_label = ctk.CTkLabel(
-            self, font=font_small, text_color=bad_color, text="error message"
+            self.footer,
+            font=font_small,
+            text_color=bad_color,
+            fg_color=inner_color,
+            text="error message",
+            justify="center",
+            corner_radius=10,
+            image=img_error,
+            compound="left",
         )
 
         # activation
         self.footer_label.grid(row=0, column=0, sticky="w")
         self.footer_entry.grid(row=0, column=1, sticky="ew")
-        self.error_label.grid(row=0, column=2, sticky="ew")
+        self.error_label.grid(row=0, column=2, sticky="", ipadx=10)
+        self.error_label.grid_remove()  # skryt error label
         # grid config
-        self.columnconfigure(0, weight=0, uniform="a")
-        self.columnconfigure(1, weight=0, uniform="b")
-        self.columnconfigure(2, weight=1, uniform="c")
-        self.rowconfigure(0, weight=0, uniform="a")
+        self.footer.columnconfigure(0, weight=0, uniform="a")
+        self.footer.columnconfigure(1, weight=0, uniform="b")
+        self.footer.columnconfigure(2, weight=1, uniform="c")
+        self.footer.rowconfigure(0, weight=0, uniform="a")
+
+    def center_window(self):  # center screen in the middle
+        """Centers the window on the screen."""
+        self.update_idletasks()
+        width: int = 1000
+        height: int = 600
+        screen_width: int = self.winfo_screenwidth()
+        screen_height: int = self.winfo_screenheight()
+        x: int = screen_width // 2 - width // 2
+        y: int = screen_height // 2 - height // 2
+        self.geometry(f"{width}x{height}+{x}+{y}")
+
+
+class TaskFrame(ctk.CTkFrame):
+    """frame na jednotlive tasky"""
+
+    def __init__(self, parent):
+        self.parent = parent.display_frame
+        super().__init__(self.parent, fg_color="white")
+        self.pack(fill="x", padx=20, pady=20)
+
+        self.task_label = ctk.CTkLabel(self, font=font_normal, text="Task 1")
+        self.status_label = ctk.CTkLabel(self, font=font_normal, text="Not Started")
+        self.checkbox = ctk.CTkLabel(self, text="| |", font=font_normal)
+
+        self.task_label.grid(row=0, column=0, sticky="ew")
+        self.status_label.grid(row=0, column=1, sticky="ew")
+        self.checkbox.grid(row=0, column=2, sticky="ew")
 
 
 if __name__ == "__main__":
