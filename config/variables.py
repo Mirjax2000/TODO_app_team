1c3291193db188e9ae8e3bcec959abbca9474a70
Vigo <miroslav.viktorin@seznam.cz>
2024-08-19 21:27:17 +0200

    pridan soubor variables

---
+++ config/variables.py
@@ -0,0 +1,26 @@
+import customtkinter as ctk
+from PIL import Image
+
+# variables
+font_big: tuple[str, int, str] = ("Arial", 30, "normal")
+font_normal: tuple[str, int, str] = ("Arial", 20, "normal")
+font_small: tuple[str, int, str] = ("Arial", 16, "normal")
+version: float = 0.5
+# images
+img_error = ctk.CTkImage(
+    light_image=Image.open("./assets/exclamation2.png"), size=(30, 30)
+)
+#colors
+inner_color: str = "#292929"
+outer_color: str = "#1c1c1c"
+bad_color: str = "#ff7f00"
+good_color: str = "#08ff00"
+btn_color_dark: str = "#14375e"
+btn_color_light: str = "#144870"
+border_color: str = "#696969"
+started_color: str = "#00519e"
+not_started_color: str = "#c86300"
+complete_color: str = "#059400"
+fg_started_color: str = "#001021"
+on_hold_color: str = "#ffee4c"
+fg_on_hold_color: str = "#241f0f"
